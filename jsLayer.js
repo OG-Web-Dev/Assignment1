f5c51ee0bd9c2ca0852c0d7157dc38ab2e6e7762
Guy Rahamim <guy.danino5@gmail.com>
2021-05-10 14:17:53 +0300

    Added basic form at the bottom.

--- jsLayer.js
+++ jsLayer.js
@@ -2,11 +2,17 @@ organization=null;
 collaboration = null;
 management=null;
 problemSolving= null;
+recommendations=null;
 
 window.onload=initElementReferences;
 
 function initElementReferences()
 {
+    script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
     organization  = document.getElementById("OrganizationBar");
     organization.style.width=0+"%";
     organization.addEventListener("mouseover",advanceProgressBar);
@@ -22,6 +28,11 @@ function initElementReferences()
     problemSolving = document.getElementById("ProblemSolvingBar");
     problemSolving.style.width=0+"%";
     problemSolving.onmouseover=advanceProgressBar;
+
+
+
+    recommendations=document.getElementById("recommendations");
+    generateForm();
 }
 
 function advanceProgressBar()
@@ -46,42 +57,79 @@ function advanceProgressBar()
 }
 
 
-// function advanceProgressBar()
-// {
-//     if (i == 0)
-//     {
-//         i = 1;
-//
-//         var width = 1;
-//         var id = setInterval(frame, 10);
-//
-//         function frame()
-//         {
-//             if (width >= 100)
-//             {
-//                 clearInterval(id);
-//                 i = 0;
-//             }
-//
-//             else
-//             {
-//                 width++;
-//                 organization.style.width = width + "%";
-//             }
-//         }
-//     }
-// }
+
+function generateFormOnBottom()
+{
+    let form=document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "submit.php");
+    form.append("body");
+    let button = document.createElement("input");
+    form.appendChild(button);
+}
+
+function generateForm()
+{
+    var down = document.getElementById("body");
+
+    // Create a break line element
+    var br = document.createElement("br");
+    GFG_Fun();
+    function GFG_Fun() {
+
+        var down = document.getElementById("GFG_DOWN");
+        GFG_Fun();
+
+        function GFG_Fun() {
+
+            // Create a form synamically
+            var form = document.createElement("form");
+            form.setAttribute("method", "post");
+            form.setAttribute("action","index.html");
+
+            //Create an input element for the name.
+            var name = document.createElement("input");
+            name.setAttribute("type","name");
+            name.setAttribute("name","name");
+            name.setAttribute("placeholder","Full name");
 
 
+            // Create an input element for emailID
+            var ID = document.createElement("input");
+            ID.setAttribute("type", "text");
+            ID.setAttribute("name", "emailID");
+            ID.setAttribute("placeholder", "E-Mail ID");
 
-// function autoFill()
-// {
-//     let width=1;
-//     organization.style.width="1%"
-//     while (organization.style.width<"100%")
-//     {
-//         console.log("filling");
-//         width++;
-//         organization.style.width=width +"%";
-//     }
-// }
+            // Create an input element for password
+            var PWD = document.createElement("input");
+            PWD.setAttribute("type", "password");
+            PWD.setAttribute("name", "password");
+            PWD.setAttribute("placeholder", "Password");
+
+            // Create a submit button
+            var s = document.createElement("input");
+            s.setAttribute("type", "submit");
+            s.setAttribute("value", "Submit");
+
+
+            form.append(name);
+            // Append the email_ID input to the form
+            form.append(ID);
+
+            // Append the password to the form
+            form.append(PWD);
+
+            // Append the button to the form
+            form.append(s);
+
+            document.getElementsByTagName("body")[0].appendChild(form);
+            let boundry= document.getElementById("boundry");
+            br= document.createElement("br");
+            br.setAttribute("type","br");
+            boundry.parentNode.insertBefore(br,boundry.nextSibling);
+
+            br.parentNode.insertBefore(form,br.nextSibling);
+            form.style.textAlign="center";
+        }
+    }
+}
