266b38005c18826af65b8c350562473c72998337
Or-Arbiv1 <62220893+Or-Arbiv1@users.noreply.github.com>
2021-05-12 19:59:20 +0300

    Merged branch (#1)
    * Minor fixes and additions:
    -Changing the color of the counter instead of showing an alert - more user friendly.
    -removed one of the boolean conditions in the scroll to top condition as it always returns false.
    
    * Encapsulated all functionality into functions for easier reading.
    
    * Encapsulated all functionality into functions for easier reading.
    
    * final web exe 2
    
    deleted comment.
    
    Co-authored-by: Guy Rahamim <guy.danino5@gmail.com>

--- JavaScriptFile.js
+++ JavaScriptFile.js
@@ -1,24 +1,37 @@
-window.onload=function () {
+window.onload=initInteractiveElements;
+window.onscroll = enableScrollToTop;
 
+function initInteractiveElements(){
+    //Moved all modal logic into an outside function.
+    handleModal();
 
+    //Moved all progress bar logic into an outside function.
+    handleBars();
+
+    //Moved all form logic into an outside function.
+    handleForm();
+
+    //moved initializing of "move to top" functionality to outside function.
+    initBackToTopListener();
+}
+function handleModal() {
     let modal = document.getElementById("myModal");
     let img = document.getElementById("info_pic");
     let modalImg = document.getElementById("img01");
     let captionText = document.getElementById("caption");
+
     img.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
     }
 
-
     let span = document.getElementsByClassName("close")[0];
-
-
     span.onclick = function () {
         modal.style.display = "none";
     }
-    //2
+}
+function handleBars() {
     let barProgress = {OrganizationBar: 85, CollaborationBar: 80, ManagementBar: 99, ProblemSolvingBar: 75};
     let barArr = document.getElementsByClassName("progress-bar");
     for (let i = 0; i < barArr.length; i++) {
@@ -31,26 +44,24 @@ window.onload=function () {
             this.style.width = j.toString() + "%";
         }
     }
-
-//3
-
+}
+function handleForm() {
     $("[id='comment']").attr("required", true);
     $("[id='first-name']").attr("required", true);
     $("[id='last-name']").attr("required", true);
     $("[id='email']").attr("required", true);
     $("[id='phone']").attr("required", true);
 
-
-
     let textarea = document.getElementById("comment");
     let current = document.getElementById("current");
+
     textarea.setAttribute("maxlength","200");
     if(/^[0-9]+$/.test(textarea.getAttribute("maxlength"))) {
-        let func = function() {
+
+        function func() {
             let len = parseInt(this.getAttribute("maxlength"), 10);
 
-            if(this.value.length == len) {
-                alert('Maximum length exceeded: ' + len);
+            if(this.value.length >= len) {
                 this.value = this.value.substr(0, len);
                 return false;
             }
@@ -58,29 +69,32 @@ window.onload=function () {
         textarea.onkeyup = func;
     }
 
-
-
     textarea.addEventListener("keyup", updateCounter);
 
     function updateCounter() {
         current.innerHTML = textarea.textLength;
-    }
+        if (textarea.textLength>=200)
+            current.style.color="red";
 
-    //6
+        else
+            current.style.color="black";
+
+    }
+}
+function initBackToTopListener(){
     let backToTop=document.getElementById("backToTopBtn");
     backToTop.addEventListener("click",topFunc);
 
     function topFunc(){
         let rootElement=document.documentElement;
-       rootElement.scrollTo({
-           top:0, behavior:"smooth"
-       })
+        rootElement.scrollTo({
+            top:0, behavior:"smooth"
+        })
     }
 }
-
-window.onscroll = function() {
-let mybutton=document.getElementById("backToTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+function enableScrollToTop(){
+    let mybutton=document.getElementById("backToTopBtn");
+    if (document.documentElement.scrollTop>20){
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
