289f1c8e3c0e12396584ac6d948e07d4d4f57f33
Guy Rahamim <guy.danino5@gmail.com>
2021-05-12 14:17:27 +0300

    Minor fixes and additions: -Changing the color of the counter instead of showing an alert - more user friendly. -removed one of the boolean conditions in the scroll to top condition as it always returns false.

--- JavaScriptFile.js
+++ JavaScriptFile.js
@@ -49,8 +49,8 @@ window.onload=function () {
         let func = function() {
             let len = parseInt(this.getAttribute("maxlength"), 10);
 
-            if(this.value.length == len) {
-                alert('Maximum length exceeded: ' + len);
+            if(this.value.length >= len) {
+                //alert('Maximum length exceeded: ' + len);
                 this.value = this.value.substr(0, len);
                 return false;
             }
@@ -59,11 +59,21 @@ window.onload=function () {
     }
 
 
-
-    textarea.addEventListener("keyup", updateCounter);
+    //***************Changed event type from keyup to keypress **********//
+    // This will make the counter react each key press, and not only with the
+    // key was released.
+    textarea.addEventListener("keypress", updateCounter);
 
     function updateCounter() {
         current.innerHTML = textarea.textLength;
+
+        //********* Changing the text color of the counter *********//
+        if (textarea.textLength>=200)
+            current.style.color="red";
+
+        else
+            current.style.color="black";
+        //********* Changing the text color of the counter *********//
     }
 
     //6
@@ -80,7 +90,9 @@ window.onload=function () {
 
 window.onscroll = function() {
 let mybutton=document.getElementById("backToTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //the first part of the boolean expression always returns false and never used.
+    //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      if (document.documentElement.scrollTop>20){
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
