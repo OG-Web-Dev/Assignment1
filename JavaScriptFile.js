dfe3a0902ceb1cc51771d63fbed491a1e6d932bb
Guy Rahamim <guy.danino5@gmail.com>
2021-05-12 15:02:58 +0300

    Encapsulated all functionality into functions for easier reading.

--- JavaScriptFile.js
+++ JavaScriptFile.js
@@ -1,24 +1,37 @@
-window.onload=function () {
+window.onload=initInteractiveElements;
+window.onscroll = enableScrollToTop;
 
+function initInteractiveElements(){
+    //Moved all modal logic into an outside function.
+    handleModal();
 
+    //Moved all progress bar logic into an outside function.
+    handleBars();
+
+    //Moved all form logic into an outside function.
+    handleForm();
+
+    //moved initializing of "move to top" functionality to outside function.
+    initBackToTopListener();
+}
+function handleModal() {
     let modal = document.getElementById("myModal");
     let img = document.getElementById("info_pic");
     let modalImg = document.getElementById("img01");
     let captionText = document.getElementById("caption");
+
     img.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
     }
 
-
     let span = document.getElementsByClassName("close")[0];
-
-
     span.onclick = function () {
         modal.style.display = "none";
     }
-    //2
+}
+function handleBars() {
     let barProgress = {OrganizationBar: 85, CollaborationBar: 80, ManagementBar: 99, ProblemSolvingBar: 75};
     let barArr = document.getElementsByClassName("progress-bar");
     for (let i = 0; i < barArr.length; i++) {
@@ -31,22 +44,21 @@ window.onload=function () {
             this.style.width = j.toString() + "%";
         }
     }
-
-//3
-
+}
+function handleForm() {
     $("[id='comment']").attr("required", true);
     $("[id='first-name']").attr("required", true);
     $("[id='last-name']").attr("required", true);
     $("[id='email']").attr("required", true);
     $("[id='phone']").attr("required", true);
 
-
-
     let textarea = document.getElementById("comment");
     let current = document.getElementById("current");
+
     textarea.setAttribute("maxlength","200");
     if(/^[0-9]+$/.test(textarea.getAttribute("maxlength"))) {
-        let func = function() {
+
+        function func() {
             let len = parseInt(this.getAttribute("maxlength"), 10);
 
             if(this.value.length >= len) {
@@ -58,11 +70,7 @@ window.onload=function () {
         textarea.onkeyup = func;
     }
 
-
-    //***************Changed event type from keyup to keypress **********//
-    // This will make the counter react each key press, and not only with the
-    // key was released.
-    textarea.addEventListener("keypress", updateCounter);
+    textarea.addEventListener("keyup", updateCounter);
 
     function updateCounter() {
         current.innerHTML = textarea.textLength;
@@ -75,24 +83,23 @@ window.onload=function () {
             current.style.color="black";
         //********* Changing the text color of the counter *********//
     }
-
-    //6
+}
+function initBackToTopListener(){
     let backToTop=document.getElementById("backToTopBtn");
     backToTop.addEventListener("click",topFunc);
 
     function topFunc(){
         let rootElement=document.documentElement;
-       rootElement.scrollTo({
-           top:0, behavior:"smooth"
-       })
+        rootElement.scrollTo({
+            top:0, behavior:"smooth"
+        })
     }
 }
-
-window.onscroll = function() {
-let mybutton=document.getElementById("backToTopBtn");
+function enableScrollToTop(){
+    let mybutton=document.getElementById("backToTopBtn");
     //the first part of the boolean expression always returns false and never used.
     //if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      if (document.documentElement.scrollTop>20){
+    if (document.documentElement.scrollTop>20){
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
